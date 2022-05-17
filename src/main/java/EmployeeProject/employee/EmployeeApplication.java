6e0f5d1dd78fffc3ef4c27819af87514123516f9
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-05-18 00:27:49 +0530

    Added:logging info in spring application

--- src/main/java/EmployeeProject/employee/EmployeeApplication.java
+++ src/main/java/EmployeeProject/employee/EmployeeApplication.java
@@ -3,11 +3,16 @@ package EmployeeProject.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeeApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
-	}
+		log.info("Logger is Activated!!");
+        System.out.println("Logger 2");
 
+}
 }
